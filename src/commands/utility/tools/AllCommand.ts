e7158caeef9f5771c5275c85089fd685337d7b92
CarlosG <cgarciagarcia729@gmail.com>
2026-03-12 03:10:45 -0600

    refactor(commands): agrupar herramientas en commands/utility/tools/ y eliminar TranslateCommand legacy

--- src/commands/utility/tools/AllCommand.ts
+++ src/commands/utility/tools/AllCommand.ts
@@ -1,4 +1,4 @@
-import { Command } from "../Command.js";
+import { Command } from "../../Command.js";
 import { CommandCategory, CommandContext } from "@/types/index.js";
 import type { MessageContext } from "@/types/index.js";
 
