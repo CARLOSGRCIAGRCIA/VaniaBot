2f2b91fd9730d7571c17f3fb9ace1db7cb2643b7
CarlosG <cgarciagarcia729@gmail.com>
2026-03-18 21:20:04 -0600

    feat: add entertainment commands (meme, chiste, 8ball, verdad, ppt, random, consejo, horoscopo, pelicula, anime, stickerrandom)

---
+++ src/commands/fun/index.ts
@@ -0,0 +1,13 @@
+export {
+  MemeCommand,
+  ChisteCommand,
+  Bola8Command,
+  VerdadRetoCommand,
+  PPTCommand,
+  RandomCommand,
+  ConsejoCommand,
+  HoroscopoCommand,
+  PeliculaCommand,
+  AnimeCommand,
+  StickerRandomCommand,
+} from './FunCommand.js';
